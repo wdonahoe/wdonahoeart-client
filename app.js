49f1dbf9bff5ab3c2c97a0fceef7ac5b1d40f841
William Donahoe <wdonahoe@udel.edu>
2015-12-12 18:16:17 -0500

    added mobile gallery

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ var app = angular.module('wdonahoeart', [
 	'wdonahoeart.landing'
 ]);
 
-app.constant('API_URL','http://localhost:8080/api')
+//app.constant('API_URL','http://localhost:8080/api')
+app.constant('API_URL','http://107.170.44.229/api')
 .config(function($urlRouterProvider, $httpProvider){
 	
 	/* note, you NEED this line for ui-router to work! */
